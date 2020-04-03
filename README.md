b4a8dbfb7f7f5cc5de22b2f767173a4fb98ea5c8
sammiefrog <sammiefrog@live.com>
2020-04-03 14:13:43 -0500

    got writefile to work

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
-# readme-generator
+
+          # asdfghjk
+    
+          ## Description
+          asdfghjkl
+    
+          ## Technology Stack
+          Node.Js,Express,JavaScript,jQuery
+    
+          ## Contributors
+          asdfghjk
+    
+          ## Contact
+          * #### Name: asdfghjkl(@asdfghjkl;)
+          * #### Portfolio: https://sammiefrog.github.io/
+          * #### Email: []()
+          * #### LinkedIn: www.linkedin.com/in/sammantha-sasenick412
+    
+          ## License
+          MIT
+        
--- index.js
+++ index.js
@@ -1,35 +1,104 @@
 const fs = require('fs');
 const inquirer = require('inquirer');
 const axios = require('axios');
-// const primeng = require('primeng');
+const datafire = require('datafire');
 
 function inquireQuestions () {
     inquirer
   .prompt([
     {
       type: "input",
-      message: "",
-      name: ""
+      message: "What is your name?",
+      name: "name"
+    },
+    {
+      type: "input",
+      message: "What is your Github Username?",
+      name: "username"
+    },
+    {
+      type: "password",
+      message: "What is your GitHub password?",
+      name: "password"
+    },
+    {
+      type: "input",
+      message: "What is your project's title?",
+      name: "project"
+    },
+    {
+      type: "input",
+      message: "Who contributed to the project?",
+      name: "contributors"
+    },
+    {
+      type: "input",
+      message: "Please enter a description of your project?",
+      name: "description"
     },
     {
       type: "checkbox",
-      message: "",
-      choices: [],
-      name: ""
+      message: "What technologies did you use?",
+      choices: ["Node.Js", "Express", "JavaScript", "jQuery", "React.js", "React", "GIT", "GitHub", "MongoDB", "MySQL", "Firebase", "Handlebars", "HTML", "CSS", "Bootstrap", "Media Queries", "APIs", "Microsoft Suite", "Heroku", "Command- Line"],
+      name: "technology"
     },
     {
       type: "list",
-      message: "",
-      choices: [],
-      name: ""
-    }
+      message: "What license do you want to use?",
+      choices: ['MIT', 'ISC', 'Apache', 'GPL', 'BSD'],
+      name: "license"
+    },
+    {
+      type: "input",
+      message: "What is you linked-in username?",
+      name: "linkedin"
+    },
+    {
+      type: "input",
+      message: "What is your portfolio link?",
+      name: "portfolio"
+    },
   ])
   .then(function(response) {
-    console.log(response);
+
+    // console.log(response);
+
+      const usersInfo = `
+          # ${response.project}
     
+          ## Description
+          ${response.description}
+    
+          ## Technology Stack
+          ${response.technology}
+    
+          ## Contributors
+          ${response.contributors}
+    
+          ## Contact
+          * #### Name: ${response.name}(@${response.username})
+          * #### Portfolio: ${response.portfolio}
+          * #### Email: []()
+          * #### LinkedIn: www.linkedin.com/in/${response.linkedin}
+    
+          ## License
+          ${response.license}
+        `
+      fs.writeFile("README.md", usersInfo, function(err) {
+        
+        if (err) {
+          return console.log(err);
+        }
+      
+        console.log("Success!");
+      
+      });
+ 
   });
   //end function
 }
+
+
 
 function githubAPICall () {
 
@@ -50,17 +119,10 @@ function githubAPICall () {
 
     //end function
 }
-githubAPICall();
+// githubAPICall();
+inquireQuestions();
 
-        // fs.writeFile("repos.txt", JSON.stringify(repos), + '/n', function(err) {
-    
-        //   if (err) {
-        //     return console.log(err);
-        //   }
-        
-        //   console.log("Success!");
-        
-        // });
+
             // fs.writeFile('log.txt', JSON.stringify(response), function(err) {
     //     if (err) {
     //         console.log('error');
