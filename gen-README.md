51993de0d9082b6b8e5e6744dc8fbfd5d5ba7af3
sammiefrog <sammiefrog@live.com>
2020-04-07 09:33:27 -0500

    fixed error with generation of readme to raw file

---
+++ gen-README.md
@@ -0,0 +1,40 @@
+# This is a Test Project
+<img align="left" src="https://img.shields.io/badge/License-MIT-green">
+<img align="right" width="100" height="100" src="https://avatars0.githubusercontent.com/u/59233248?v=4">
+  
+## Contributors
+@sammiefrog @jeverett93 @chaalexander @git-theresa
+    
+## Table of Contents
+<li><a href="#description">Description</a></li>  
+<li><a href="#installation">Installation</a></li> 
+<li><a href="#tech">Technology Stack</a></li> 
+<li><a href="#usage">Usage</a></li> 
+<li><a href="#contact">Contact</a></li> 
+<li><a href="#license">License</a></li> 
+<li><a href="#tests">Tests</a></li> 
+  
+<h2 id= "description">Description</h2>
+Testing readme generator.
+  
+<h2 id= "installation">Installation</h2>
+npm i, obtain github token
+    
+<h2 id= "technology">Technology Stack</h2>
+ Node.Js, JavaScript, GIT
+  
+<h2 id= "usage">Usage</h2>
+Generating readme's quickly.
+  
+<h2 id= "contact">Contact</h2>
+<li>Name: Sammantha Sasenick</li> 
+<li>Github: @[sammiefrog](https://github.com/sammiefrog)</li> 
+<li>Portfolio: [sammiefrog.github.io](sammiefrog.github.io)</li> #### Portfolio: [sammiefrog.github.io](sammiefrog.github.io)
+<li>Email: [ssasenick412@gmail.com](ssasenick412@gmail.com)</li> 
+<li>LinkedIn: www.linkedin.com/in/sammantha-sasenick412</li> 
+    
+<h2 id= "license">License</h2>
+MIT
+  
+<h2 id= "tests">Tests</h2>
+testing, testing... 
--- generateMD.js
+++ generateMD.js
@@ -1,50 +1,48 @@
 const fs = require('fs');
 
 const generate = { generateMD(res, response) {
-    const usersInfo = `
+
+    const usersInfo = `# ${response.project}
+<img align="left" src="https://img.shields.io/badge/License-${response.license}-green">
+<img align="right" width="100" height="100" src="${res.data.avatar_url}">
   
-    # ${response.project}
-    <img align="left" src="https://img.shields.io/badge/License-${response.license}-green">
-    <img align="right" width="100" height="100" src="${res.data.avatar_url}">
-  
-    ## Contributors
-    ${response.contributors}
+## Contributors
+${response.contributors}
     
-    ## Table of Contents
-    <li><a href="#description">Description</a></li>  
-    <li><a href="#installation">Installation</a></li> 
-    <li><a href="#tech">Technology Stack</a></li> 
-    <li><a href="#usage">Usage</a></li> 
-    <li><a href="#contact">Contact</a></li> 
-    <li><a href="#license">License</a></li> 
-    <li><a href="#tests">Tests</a></li> 
+## Table of Contents
+<li><a href="#description">Description</a></li>  
+<li><a href="#installation">Installation</a></li> 
+<li><a href="#tech">Technology Stack</a></li> 
+<li><a href="#usage">Usage</a></li> 
+<li><a href="#contact">Contact</a></li> 
+<li><a href="#license">License</a></li> 
+<li><a href="#tests">Tests</a></li> 
   
-    <h2 id= "description">Description</h2>
-    ${response.description}
+<h2 id= "description">Description</h2>
+${response.description}
   
-    <h2 id= "installation">Installation</h2>
-    ${response.installation}
+<h2 id= "installation">Installation</h2>
+${response.installation}
     
-    <h2 id= "technology">Technology Stack</h2>
-    ${response.technology}
+<h2 id= "technology">Technology Stack</h2>
+${response.technology}
   
-    <h2 id= "usage">Usage</h2>
-    ${response.usage}
+<h2 id= "usage">Usage</h2>
+${response.usage}
   
-    <h2 id= "contact">Contact</h2>
-    <li>Name: ${res.data.name}</li> 
-    <li>Github: @[${response.username}](${res.data.html_url})</li> 
-    <li>Portfolio: [${response.portfolio}](${response.portfolio})</li> #### Portfolio: [${response.portfolio}](${response.portfolio})
-    <li>Email: [${res.data.email}](${res.data.email})</li> 
-    <li>LinkedIn: www.linkedin.com/in/${response.linkedin}</li> 
+<h2 id= "contact">Contact</h2>
+<li>Name: ${res.data.name}</li> 
+<li>Github: @[${response.username}](${res.data.html_url})</li> 
+<li>Portfolio: [${response.portfolio}](${response.portfolio})</li> #### Portfolio: [${response.portfolio}](${response.portfolio})
+<li>Email: [${res.data.email}](${res.data.email})</li> 
+<li>LinkedIn: www.linkedin.com/in/${response.linkedin}</li> 
     
-    <h2 id= "license">License</h2>
-    ${response.license}
+<h2 id= "license">License</h2>
+${response.license}
   
-    <h2 id= "tests">Tests</h2>
-    ${response.tests}
-    `
-      fs.writeFile("README.md", usersInfo, function (err) {
+<h2 id= "tests">Tests</h2>
+${response.tests} `
+      fs.writeFile("gen-README.md", usersInfo, function (err) {
   
         if (err) {
             return console.log(err);
